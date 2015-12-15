023f80d076b999eec0708a66c258a9b543d3b54d
Atul Sandur <athuls89@gmail.com>
2015-12-15 00:25:42 -0600

    Heuristics- part 1

---
+++ tool/src/constraint/QP.py
@@ -0,0 +1,57 @@
+'''
+Created on Dec 8, 2015
+
+@author: sandur2
+[This class is not ready to use]
+'''
+
+from cvxopt import matrix, solvers
+
+class QP:
+    '''
+    classdocs
+    '''
+    
+    def __init__(self, clist=None):
+        '''
+        Constructor
+        '''
+        self.constraintList=[]
+        if not(clist is None):
+            self.add_constraint_list(clist)
+        
+    def add_constraint(self, constraint):
+        constraint.check_dim(self.num_vars)
+        self.constraintList.append(constraint)
+        
+    def add_constraint_list(self, clist):
+        for c in clist:
+            self.add_constraint(c)
+        
+    def get_LP(self):
+        A = []
+        b = []
+        for c in self.constraintList:
+            A.append(c.coefficients)
+            b.append(c.constant)
+        return (A,b)
+    
+    def to_string(self):
+        return '\n'.join(self.constraintList)
+    
+    def __str__(self):
+        return self.to_string()
+
+    def __repr__(self):
+        return self.__str__()
+
+    def solve(self,objective):
+        '''Return solution of LP'''
+        '''optimizing dummy variable for LP'''
+        c = matrix(objective)
+        [A,b] = self.get_LP()
+        AMatrix = matrix(A)
+        AMatrix = AMatrix.trans()
+        bMatrix = matrix(b).trans()
+        sol = solvers.lp(c, AMatrix, bMatrix)
+        return sol
---
+++ tool/src/heuristic/StochasticTestSuite.py
@@ -0,0 +1,46 @@
+'''
+Created on Jun 17, 2011
+
+@author: Sai Panyam
+
+Unit tests that exercise the algorithms. It is a mixture of both real 'unit' tests and functional tests of search
+'''
+import unittest
+from .ResultHelpers import TSPResult.FormattedOutput
+class SearchTests(unittest.TestCase):
+    
+    def setUp(self):
+        self.Vector = [1,2]
+        # Problem Configuration
+        berlin52 = [[565,575],[25,185],[345,750],[945,685],[845,655],
+                    [880,660],[25,230],[525,1000],[580,1175],[650,1130],[1605,620],
+                    [1220,580],[1465,200],[1530,5],[845,680],[725,370],[145,665],
+                    [415,635],[510,875],[560,365],[300,465],[520,585],[480,415],
+                    [835,625],[975,580],[1215,245],[1320,315],[1250,400],[660,180],
+                    [410,250],[420,555],[575,665],[1150,1160],[700,580],[685,595],
+                    [685,610],[770,610],[795,645],[720,635],[760,650],[475,960],
+                    [95,260],[875,920],[700,500],[555,815],[830,485],[1170,65],
+                    [830,610],[605,625],[595,360],[1340,725],[1740,245]
+                   ]
+        self.TSPLIB = berlin52
+        
+    def tearDown(self):
+        self.Vector =[]
+        
+    #@unittest.skip("Don't run FOR NOW!")          
+    def testTabuSearch(self):
+        from .TabuSearch import search
+        # Problem Configuration
+        # Use Berlin52 instance of TSPLIB
+        # Algorithm Configuration
+        maxIterations = 100
+        maxTabuCount = 15
+        maxCandidates = 50
+        # Execute the algorithm
+        result = search(self.TSPLIB,maxIterations, maxTabuCount, maxCandidates)
+        tspResult = TSPResult(7542,"Tabu Search Results")
+        print tspResult.FormattedOutput(result)
+    
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testRandomSeaarch']
+    unittest.main()
